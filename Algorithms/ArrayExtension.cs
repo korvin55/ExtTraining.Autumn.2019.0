c3dfbe14ee03e3181056a86e579deb436df6356e
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2019-09-15 18:12:42 +0300

    Update ArrayExtension.cs

--- Algorithms/ArrayExtension.cs
+++ Algorithms/ArrayExtension.cs
@@ -4,10 +4,6 @@ namespace Algorithms
 {
     public static class ArrayExtension
     {
-        public static void BubbleSort(int[] array) => throw new NotImplementedException();
 
-        public static void QuickSort(int[] array) => throw new NotImplementedException();
-
-        public static void MergeSort(int[] array) => throw new NotImplementedException();
     }
 }
