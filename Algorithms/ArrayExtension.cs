5fbcf01060d4b66d892eaa06ef05cca30658f7f9
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2019-09-15 17:36:49 +0300

    Add project files.

---
+++ Algorithms/ArrayExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public static class ArrayExtension
+    {
+        public static void BubbleSort(int[] array) => throw new NotImplementedException();
+
+        public static void QuickSort(int[] array) => throw new NotImplementedException();
+
+        public static void MergeSort(int[] array) => throw new NotImplementedException();
+    }
+}
