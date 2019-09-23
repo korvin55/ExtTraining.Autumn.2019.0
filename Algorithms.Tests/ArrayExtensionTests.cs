ab3ce0eb0cc4ace67c5a9f26a6b0fc60ebebf388
korvin <47597209+korvin55@users.noreply.github.com>
2019-09-23 23:22:22 +0300

    version 1.2 +Iterator, + good BubbleSort

--- Algorithms.Tests/ArrayExtensionTests.cs
+++ Algorithms.Tests/ArrayExtensionTests.cs
@@ -5,14 +5,34 @@ namespace Algorithms.Tests
     [TestFixture]
     public class ArrayExtensionTests
     {
-        [TestCase(new int[]{3, 2}, 2, ExpectedResult = new int[] {2, 3})]
-        [TestCase(new int[] { 63, 56}, 8, ExpectedResult = new int[] {63, 56})]
+        [TestCase(new int[]{3, 2, 8}, 2, 1, 0, 2, 1, ExpectedResult = new int[] {2, 8, 3})]
+        [TestCase(new int[] { 63, 56}, 8, 7, 0, 1, 1, ExpectedResult = new int[] {56, 63})]
         
-        
-        public int[] TestBubbleSort(int[] array, int system)
+        public int[] TestBubbleSort(int[] array, int system, int flag, int start, int end, int step)
         {
-            IComparer comparer = new NumberSystemComparator(system);
-            ArrayExtension.BubbleSort(array, comparer);
+            IComparer comparer = new NumberSystemComparator(system, flag);
+            IIterable iterator = new Iterator(start, end, step);
+            ArrayExtension.BubbleSort(array, comparer, iterator);
+            return array;
+        }
+
+        [TestCase(new int[] { 3, 2 }, 2, 1, ExpectedResult = new int[] { 2, 3 })]
+        [TestCase(new int[] { 63, 56 }, 8, 7, ExpectedResult = new int[] { 56, 63 })]
+
+        public int[] TestQuickSort(int[] array, int system, int flag)
+        {
+            IComparer comparer = new NumberSystemComparator(system, flag);
+            ArrayExtension.QuickSort(array, comparer);
+            return array; 
+        }
+
+        [TestCase(new int[] { 3, 2 }, 2, 1, ExpectedResult = new int[] { 2, 3 })]
+        [TestCase(new int[] { 63, 56 }, 8, 7, ExpectedResult = new int[] { 56, 63 })]
+
+        public int[] TestInsertionSort(int[] array, int system, int flag)
+        {
+            IComparer comparer = new NumberSystemComparator(system, flag);
+            ArrayExtension.InsertionSort(array, comparer);
             return array;
         }
     }
--- Algorithms/ArrayExtension.cs
+++ Algorithms/ArrayExtension.cs
@@ -4,23 +4,29 @@ namespace Algorithms
 {
     public static class ArrayExtension
     {
-        static void Swap(ref int first,ref int second)
+        static void Swap(ref int first, ref int second)
         {
             var temp = first;
             first = second;
             second = temp;
         }
 
-        public static void BubbleSort( int[] array, IComparer comparator)
+        public static void BubbleSort(int[] array, IComparer comparator, IIterable iterator)
         {
-            for (int i = 0; i < array.Length; i++)
+            bool isSort = true;
+            while (isSort)
             {
-                for (int j = i + 1; j < array.Length; j++)
+                isSort = false;
+                for (int current = iterator.GetStart(); !iterator.IsEnd(); current = iterator.GetCurrent())
                 {
-                    if (comparator.Compare(array[i], array[j]) > 0)
+                    
+                    int next = iterator.GetNext();
+                    if (comparator.Compare(array[current], array[next]) > 0)
                     {
-                        Swap(ref array[i],ref array[j]);
+                        Swap(ref array[current], ref array[next]);
+                        isSort = true;
                     }
+                    iterator.Next();
                 }
             }
         }
@@ -63,11 +69,11 @@ namespace Algorithms
 
         public static void InsertionSort(int[] array, IComparer comparator)
         {
-            for (var i = 1; i < array.Length; i++)
+            for (int i = 1; i < array.Length; i++)
             {
-                var key = array[i];
-                var j = i;
-                while ((j >= 1) && (comparator.Compare(array[j-1], key) > 0))
+                int key = array[i];
+                int j = i;
+                while ((j >= 1) && (comparator.Compare(array[j - 1], key) > 0))
                 {
                     Swap(ref array[j - 1], ref array[j]);
                     j--;
@@ -95,39 +101,107 @@ namespace Algorithms
     public class NumberSystemComparator : IComparer
     {
         private int system;
+        private int flag;
 
-        public NumberSystemComparator(int system)
+        public NumberSystemComparator(int system, int flag)
         {
             this.system = system;
+            this.flag = flag;
         }
 
         public int Compare(int first, int second)
         {
-            return NumberSystem.Convert(first, system) - NumberSystem.Convert(second, system);
+            return NumberSystem.Convert(first, system, flag) - NumberSystem.Convert(second, system, flag);
         }
     }
 
     public class NumberSystem
     {
-        //doesn't work correctly
-        public static int Convert(int number,int system)
+        public static int Convert(int number, int system, int flag)
         {
             int sum = 0;
 
-            if (number <= 0 || system < 2)
+
+            while (Math.Abs(number) > 1)
             {
-                return sum;
-            }
-            
-            while (number > 1)
-            {
-                if ( number % system > 0)
+                if (Math.Abs(number) % system == flag)
                 {
                     sum++;
                 }
                 number /= system;
             }
             return sum;
+        }
+    }
+
+    public interface IIterable
+    {
+        int GetStart();
+        int GetCurrent();
+        void Next();
+        int GetNext();
+        int GetPreview();
+        bool IsEnd();
+    }
+
+    public class Iterator : IIterable
+    {
+        private int start;
+        private int end;
+        private int step;
+        private int current;
+
+        public Iterator(int start, int end, int step)
+        {
+            this.start = start;
+            this.end = end;
+            this.step = step;
+            current = start;
+        }
+
+        public int GetStart()
+        {
+            current = start;
+            return start;
+        }
+        public int GetCurrent()
+        {
+            return current;
+        }
+
+        public void Next()
+        {
+            if (start + step <= end)
+            {
+                current += step;
+            }
+        }
+
+        public int GetNext()
+        {
+            if (start + step <= end)
+            {
+                return current + step;
+            }
+            return current;
+        }
+
+        public int GetPreview()
+        {
+            if(current - step >= start)
+            {
+                return current - step;
+            }
+            return current;
+        }
+
+        public bool IsEnd()
+        {
+            if( current >= end)
+            {
+                return true;
+            }
+            return false;
         }
     }
 
